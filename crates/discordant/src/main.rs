eda9b6943c01b2de2ddf88b773fcfafb8bdfdd09
Kixiron <contact.kixiron@gmail.com>
2019-08-30 15:21:15 -0500

    Moved to hyper-tls for https requests and can now download files

--- crates/discordant/src/main.rs
+++ crates/discordant/src/main.rs
@@ -26,7 +26,6 @@ fn main() {
 
                 match message {
                     BackendMsg::MessageAdd(msg) => {
-                        println!("{:?}", msg);
                         for attachment in msg.attachments {
                             println!("Sending {} to be downloaded", attachment.proxy_url);
                             url_sender
@@ -65,36 +64,32 @@ fn backend() -> (
 }
 
 async fn async_backend(mut url_recv: Receiver<String>, file_sender: Sender<Vec<u8>>) {
-    use futures::{stream::{TryStreamExt, StreamExt}, compat::{Future01CompatExt, Stream01CompatExt}, sink::SinkExt};
+    use futures::{stream::{TryStreamExt, StreamExt}, sink::SinkExt};
     use std::str::FromStr;
     use hyper::{client::Client, Uri};
 
-    // let https = hyper_tls::HttpsConnector::new(4).unwrap();
-    // let client = Client::builder()
-    //     .build::<_, hyper::Body>(https);
-    let client = Client::new();
+    let https = hyper_tls::HttpsConnector::new().unwrap();
+    let client = Client::builder()
+        .build::<_, hyper::Body>(https);
 
     while let Some(url) = url_recv.next().await {
-        println!("Received {} to be downloaded", url);
         let client = client.clone();
-        let url = Uri::from_str(&url.replace("https://", "http://")).expect("Failed to parse URL");
+        let url = Uri::from_str(&url).expect("Failed to parse URL");
 
         let mut file_sender = file_sender.clone();
 
         tokio::spawn(async move {
-            let file = match client.get(url).compat().await {
+            let file = match client.get(url).await {
                 Ok(file) => file,
                 Err(err) => {
-                    println!("Http Error: {:?}", err);
+                    eprintln!("Http Error: {:?}", err);
                     return;
                 }
             }
             .into_body()
-            .compat()
             .try_concat()
             .await;
 
-            println!("Processed file: {:?}", file);
             file_sender
                 .send(file.unwrap().to_vec())
                 .await
